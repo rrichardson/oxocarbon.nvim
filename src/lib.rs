4a0c9696b6464f6b049eb0c5aa2d1adbea9c4bb9
Rick Richardson <ririchardson@tesla.com>
2022-11-18 09:06:24 -0800

    tweaking the number 9 color blue to be less intense

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ fn oxocarbon() -> oxi::Result<()> {
     let oxocarbon: [&str; 18] = match api::get_option::<String>("background").unwrap().as_str() {
         "dark" => [
             "#161616", "#262626", "#393939", "#525252", "#dde1e6", "#f2f4f8", "#ffffff", "#08bdba",
-            "#3ddbd9", "#78a9ff", "#ee5396", "#33b1ff", "#ff7eb6", "#42be65", "#be95ff", "#82cfff",
+            "#3ddbd9", "#78a9dd", "#ee5396", "#33b1ff", "#ff7eb6", "#42be65", "#be95ff", "#82cfff",
             "#131313", "",
         ],
         "light" => [
